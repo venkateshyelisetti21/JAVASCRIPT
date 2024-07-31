64f0d692d37760fa7b6183eac0f9c3ab72a0eb83
VENKATESH YELISETTI <109537237+venkateshyelisetti21@users.noreply.github.com>
2024-07-31 08:58:47 +0530

    via VScode

---
+++ DOM.js
@@ -0,0 +1,5 @@
+ function manipulateStyles()
+ {
+    document.getElementById('headingElement').textContent='4.O Technologies';   //For changing text
+    document.getElementById('headingElement').style.color="blue";    //For changing color of text   
+ }
---
+++ Variable-Declaration.js
@@ -0,0 +1,3 @@
+let message; //Declaration
+message = 'I am Venky'; // '=' is Assignment operator
+console.log(message); //Output statement
